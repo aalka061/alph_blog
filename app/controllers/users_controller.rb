ecfb3d6e4c471cb9a2b45afcb743249eb3c380a8
aalkaf_Rails <a_algg@hotmail.com>
2018-04-15 18:44:23 -0400

    add authemtication methods and display login&logout

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,11 @@
 class UsersController< ApplicationController
+  
+ ##before_action :set_user, only: [:edit, :show, :update]
+  
   def new 
-    @user = User.new
+   @user = User.new
   end
 
-  
   def create 
     @user = User.new(user_params)
     if @user.save 
@@ -15,11 +17,9 @@ class UsersController< ApplicationController
   end
   
   def edit
-    @user = User.find(params[:id])
   end 
   
   def update
-    @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:success] = "Your account has been successfully updated"
       redirect_to articles_path
@@ -29,7 +29,6 @@ class UsersController< ApplicationController
   end
   
   def show
-    @user = User.find(params[:id])
     @user_articles = @user.articles.paginate(page: params[:page], per_page: 5)
   end 
   
@@ -42,6 +41,9 @@ class UsersController< ApplicationController
   def user_params
     params.require(:user).permit(:username, :email, :password)
   end
-
+  
+  def set_user
+    @user = User.find(params[:id])
+  end
 
 end
